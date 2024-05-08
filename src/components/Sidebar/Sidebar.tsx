886fe37be92d63adabb79598dd04f54dda84508d
Dmytro Slipchenko <slipdem@users.noreply.github.com>
2024-05-09 02:18:28 +0300

    feat: хедер, додав перевірки для навігації чи активна сторніка, додав лого, доробив сайдбар

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,15 +4,354 @@ export const Sidebar = () => {
 			<div className='sidebar'>
 				<h1>Каталог товарів</h1>
 				<ul className='category-list'>
-					<li>Передпокій</li>
-					<li>Вітальні</li>
-					<li>Спальні</li>
-					<li>Кухоні мебелі</li>
-					<li>Дитячі мебелі</li>
-					<li>Офісні мебелі</li>
-					<li>Ванна кімната</li>
-					<li>Гардеробні</li>
-					<li>Усі товари</li>
+					<li className='category-list__item'>
+						<a
+							className='category-list__link'
+							href='/'>
+							Передпокій
+						</a>
+						<div className='category-list__hidden-column'>
+							<ul className='subcategory-list'>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Гардероб
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Дзеркала
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Стінки і гарнітури
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Шафи
+									</a>
+								</li>
+							</ul>
+						</div>
+					</li>
+					<li className='category-list__item'>
+						<a
+							className='category-list__link'
+							href='/'>
+							Вітальня
+						</a>
+						<div className='category-list__hidden-column'>
+							<ul className='subcategory-list'>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Дивани
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Крісла
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Стелажі, пенали, полиці
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Столи
+									</a>
+								</li>
+							</ul>
+						</div>
+					</li>
+					<li className='category-list__item'>
+						<a
+							className='category-list__link'
+							href='/'>
+							Спальня
+						</a>
+						<div className='category-list__hidden-column'>
+							<ul className='subcategory-list'>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Комоди
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Ліжка
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Матраци і аксесуари
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Тумби
+									</a>
+								</li>
+							</ul>
+						</div>
+					</li>
+					<li className='category-list__item'>
+						<a
+							className='category-list__link'
+							href='/'>
+							Кухонні меблі
+						</a>
+						<div className='category-list__hidden-column'>
+							<ul className='subcategory-list'>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Стільці
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Столи
+									</a>
+								</li>
+							</ul>
+						</div>
+					</li>
+					<li className='category-list__item'>
+						<a
+							className='category-list__link'
+							href='/'>
+							Дитяча кімната
+						</a>
+						<div className='category-list__hidden-column'>
+							<ul className='subcategory-list'>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Ліжка
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Комоди
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Матраци і аксесуари
+									</a>
+								</li>
+							</ul>
+						</div>
+					</li>
+					<li className='category-list__item'>
+						<a
+							className='category-list__link'
+							href='/'>
+							Офісні меблі
+						</a>
+						<div className='category-list__hidden-column'>
+							<ul className='subcategory-list'>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Дивани
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Крісла
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Столи
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Шафи
+									</a>
+								</li>
+							</ul>
+						</div>
+					</li>
+					<li className='category-list__item'>
+						<a
+							className='category-list__link'
+							href='/'>
+							Ванна кімната
+						</a>
+						<div className='category-list__hidden-column'>
+							<ul className='subcategory-list'>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Дзеркала
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Тумби
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Шафи
+									</a>
+								</li>
+							</ul>
+						</div>
+					</li>
+
+					<li className='category-list__item'>
+						<a
+							className='category-list__link'
+							href='/'>
+							Усі категорії
+						</a>
+						<div className='category-list__hidden-column'>
+							<ul className='subcategory-list'>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Гардероб
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Дзеркала
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Стінки і гарнітури
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Шафи
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Дивани
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Крісла
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Стелажі, пенали, полиці
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Столи
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Комоди
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Ліжка
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Матраци і аксесуари
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Тумби
+									</a>
+								</li>
+								<li>
+									<a
+										className='subcategory-list__link'
+										href='/'>
+										Стільці
+									</a>
+								</li>
+							</ul>
+						</div>
+					</li>
 				</ul>
 			</div>
 		</div>
