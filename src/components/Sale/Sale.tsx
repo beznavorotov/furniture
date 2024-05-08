4436f80182d0683d49543bc2ba12d278e0dbcfe0
beznavorotov <beznavorotov@gmail.com>
2024-05-08 11:51:37 +0300

    оновив sidebar i static

--- src/components/Sale/Sale.tsx
+++ src/components/Sale/Sale.tsx
@@ -1,13 +1,12 @@
 export const Sale = () => {
-	return (
-		<div>
-			<div className='sale'>
-				<div className='collection box-1'>box-1</div>
-				<div className='collection box-2'>box-2</div>
-				<div className='collection box-3'>box-3</div>
-				<div className='collection box-4'>box-4</div>
-			</div>
-			Sale1
-		</div>
-	);
+  return (
+    <div>
+      <div className="sale">
+        <div id="item-0">Box - 1</div>
+        <div id="item-1">Box - 2</div>
+        <div id="item-2">Box - 3</div>
+        <div id="item-3">Box - 4</div>
+      </div>
+    </div>
+  );
 };
