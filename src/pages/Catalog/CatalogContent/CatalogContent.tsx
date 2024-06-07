cc16021b463c500e0bc7124ac4316b43b1d1303f
Dmytro Slipchenko <dslipchenko@gmail.com>
2024-06-08 00:29:04 +0300

    Update CatalogContent.tsx

--- src/pages/Catalog/CatalogContent/CatalogContent.tsx
+++ src/pages/Catalog/CatalogContent/CatalogContent.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import {  useState } from 'react';
 import { useSelector } from 'react-redux';
 import ProductCard from '../../../components/ProductCard/ProductCard';
 import { RootState } from '../../../store';
