84b14dfce44f633ec0c795a06ca4753a6e81ca4a
beznavorotov <111625937+beznavorotov@users.noreply.github.com>
2024-06-13 14:11:57 +0300

    Update ScrollToTop.js

--- src/utils/ScrollToTop.js
+++ src/utils/ScrollToTop.js
@@ -1,9 +1,7 @@
-function scrollToTop(behavior) {
-  return window.scrollTo({
+export default function scrollToTop(behavior = 'smooth') {
+  window.scrollTo({
     top: 0,
     left: 0,
     behavior: behavior,
   });
 }
-
-export default scrollToTop;
