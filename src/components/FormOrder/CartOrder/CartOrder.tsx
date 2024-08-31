b82b64aebf7d78d3bcd232ea16bb8dec8fec7522
Dmytro Slipchenko <dslipchenko@gmail.com>
2024-08-31 12:18:31 +0300

    Update CartOrder.tsx

--- src/components/FormOrder/CartOrder/CartOrder.tsx
+++ src/components/FormOrder/CartOrder/CartOrder.tsx
@@ -7,47 +7,41 @@ export const CartOrder = () => {
   };
 
   const { cartProducts, totalQuantity, totalPrice } = cartData;
-
   return (
-    <div className="cart_summary">
+    <div className='cart_summary'>
       {cartProducts.length > 0 ? (
         cartProducts.map((product) => (
-          <div key={product.article} className="product_details">
-            <img
-              src={product.image}
-              alt={product.name}
-              className="icon_product"
-            />
-            <div className="cart_details">
-              <span className="name_product">{product.name}</span>
-              <span className="article_product">
-                Код товару: {product.article}
+          <div key={product.item_cart_id} className='product_details'>
+            <div className='cart_details'>
+              <span className='name_product'>{product.item.title}</span>
+              <span className='article_product'>
+                Код товару: {product.item.article_code}
               </span>
             </div>
-            <div className="quantity_details">
-              <span className="quantity_product">
+            <div className='quantity_details'>
+              <span className='quantity_product'>
                 Кількість: {product.quantity}
               </span>
-              <span className="product_price">{product.price} грн</span>
+              <span className='product_price'>{product.item.price} грн</span>
             </div>
           </div>
         ))
       ) : (
         <p>Кошик порожній</p>
       )}
-      <div className="basket_summary">
-        <div className="summary_details">
-          <div className="summary_item">
-            <span className="text">Товарів: </span>
-            <span className="text">{totalQuantity}</span>
+      <div className='basket_summary'>
+        <div className='summary_details'>
+          <div className='summary_item'>
+            <span className='text'>Товарів: </span>
+            <span className='text'>{totalQuantity}</span>
           </div>
-          <div className="summary_item">
-            <span className="text">Сума товарів:</span>
-            <span className="text">{totalPrice} грн</span>
+          <div className='summary_item'>
+            <span className='text'>Сума товарів:</span>
+            <span className='text'>{totalPrice} грн</span>
           </div>
-          <div className="summary_item">
-            <span className="text_all">Всього:</span>
-            <span className="text_all">{totalPrice} грн</span>
+          <div className='summary_item'>
+            <span className='text_all'>Всього:</span>
+            <span className='text_all'>{totalPrice} грн</span>
           </div>
         </div>
       </div>
